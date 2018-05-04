a0cfcae99c8d696d4f573078770304f4cb6a8188
fradp <36346451+fradp@users.noreply.github.com>
2018-05-04 10:25:53 +0200

    Iniziata rubrica

---
+++ rubrica.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Benvenuto su DprBand!</title>
+</head>
+<body>
+  <?php
+
+	echo "<details>
+		   <summary>Prova Lista</summary>
+		   <p>Primo</p>
+		   <p>Secondo</p>
+		  </details>";
+  ?>
+</body>
+</html>
