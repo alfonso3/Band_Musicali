0d5792bd6c3a18246fd1aaa5da094acb0cef0d27
Vincenzo Romito <romito_vincenzo@libero.it>
2018-05-06 19:26:20 +0200

    Pagina storico completamente funzionante, da aggiungere CSS.

--- storico.php
+++ storico.php
@@ -14,11 +14,11 @@ if(isset($_SESSION['username']))
     	die("Connection failed: " . mysqli_connect_error());
 	}
 	else
-	{
+	{	
 		
-					if(!isset($_SESSION['ordine']))
-						{
-							//recupera dati eventi passati
+
+		if(!isset($_POST['button']))
+		{
 							$sql1 = "SELECT rlocale.Nome, rlocale.Citta, rconcerto.Data, rconcerto.Ora, rconcerto.CompensoPattuito, rconcerto.CompensoEffettivo FROM rconcerto INNER JOIN rlocale ON rconcerto.ID_Locale = rlocale.ID WHERE rconcerto.CompensoEffettivo!='0'";
 
 							$result = mysqli_query($conn, $sql1);
@@ -47,82 +47,168 @@ if(isset($_SESSION['username']))
 							echo "0 results";
 							}
 
+							echo"Eventi in ordine casuale";
 
 							for($i=0;$i<$nEventi;$i++)
 							{
-								echo"<details>
+								
+								echo"	<form action=\"storico.php\" method=\"POST\">
+										<details>
 										<summary>".$cittaLocale[$i]." ".$dataConcerto[$i]."</summary>
 										<div>Ora di inizio: ".$oraConcerto[$i]."</div>
 										<div>Locale: ".$nomeLocale[$i]."</div>
 										<div>Compenso pattuito: ".$compensoPattuitoConcerto[$i]."</div>
 										<div>Compenso effettivo ricevuto: ".$compensoEffettivoConcerto[$i]."</div>
-									</details>";
+										</details>
+										";
 							}
 
 
 
 							echo"Ordina per guadagno: ";
-							echo"<select name=\"ordinamento\" id=\"ordinamento\" onChange=\"ordinamentoEventi()\">
-									  <option value=\"\"></option>
+							echo"<select name=\"ordinamento\">
+									  <option value=\"casuale\">-casuale-</option>
 							 		  <option value=\"crescente\">Crescente</option>
 									  <option value=\"decrescente\">Decrescente</option>
-								</select>";
-			    		}
-			    		else
-			    		{
-			    			if($_SESSION['ordine'] == "crescente")
-			    			{
-			    					
+								</select>
+									  <input type=\"submit\" name=\"button\" value=\"ricerca\"></form>";
+					
+		}
+		else
+		{	
+	
+			$ordinamento = $_POST["ordinamento"];
 
-			    					
+							if($ordinamento == "crescente")
+			    			{
+
+			    				$sql2 = "SELECT rlocale.Nome, rlocale.Citta, rconcerto.Data, rconcerto.Ora, rconcerto.CompensoPattuito, rconcerto.CompensoEffettivo FROM rconcerto INNER JOIN rlocale ON rconcerto.ID_Locale = rlocale.ID WHERE rconcerto.CompensoEffettivo!='0' ORDER BY CompensoEffettivo ASC";
+
+							$result = mysqli_query($conn, $sql2);
+
+							if(mysqli_num_rows($result) > 0 )
+							{
+								$nEventi = mysqli_num_rows($result);
+								$nomeLocale[] = '';
+								$cittaLocale[] = '';
+								$dataConcerto[] = '';
+								$oraConcerto[] = '';
+								$compensoPattuitoConcerto[] = '';
+								$compensoEffettivoConcerto[] = '';
+								for($i = 0;$row = mysqli_fetch_assoc($result); $i++)
+								{
+									$nomeLocale[$i] = $row["Nome"];
+									$cittaLocale[$i] = $row["Citta"];
+									$dataConcerto[$i] = $row["Data"];
+									$oraConcerto[$i] = $row["Ora"];
+									$compensoPattuitoConcerto[$i] = $row["CompensoPattuito"];
+									$compensoEffettivoConcerto[$i] = $row["CompensoEffettivo"];
+								}
+							}
+							else
+							{
+							echo "0 results";
+							}
+			    			
+							echo"Eventi in ordine di guadagno crescente";
+
+			    			for($i=0;$i<$nEventi;$i++)
+							{
+
+								echo"	<form action=\"storico.php\" method=\"POST\">
+										<details>
+										<summary>".$cittaLocale[$i]." ".$dataConcerto[$i]."</summary>
+										<div>Ora di inizio: ".$oraConcerto[$i]."</div>
+										<div>Locale: ".$nomeLocale[$i]."</div>
+										<div>Compenso pattuito: ".$compensoPattuitoConcerto[$i]."</div>
+										<div>Compenso effettivo ricevuto: ".$compensoEffettivoConcerto[$i]."</div>
+										</details>
+										";
+							}
+
+
+
+							echo"Ordina per guadagno: ";
+							echo"<select name=\"ordinamento\">
+									  <option value=\"casuale\">-casuale-</option>
+							 		  <option value=\"crescente\">Crescente</option>
+									  <option value=\"decrescente\">Decrescente</option>
+								</select>
+									  <input type=\"submit\" name=\"button\" value=\"ricerca\"></form>";
+					
+			    			
 			    			}
 
-			    			if($_SESSION['ordine'] == "decrescente")
+			    			if($ordinamento == "decrescente")
 			    			{
-			    				
-			    			}	
-			    		}
-		    
+			    				$sql3 = "SELECT rlocale.Nome, rlocale.Citta, rconcerto.Data, rconcerto.Ora, rconcerto.CompensoPattuito, rconcerto.CompensoEffettivo FROM rconcerto INNER JOIN rlocale ON rconcerto.ID_Locale = rlocale.ID WHERE rconcerto.CompensoEffettivo!='0' ORDER BY CompensoEffettivo DESC";
+
+							$result = mysqli_query($conn, $sql3);
+
+							if(mysqli_num_rows($result) > 0 )
+							{
+								$nEventi = mysqli_num_rows($result);
+								$nomeLocale[] = '';
+								$cittaLocale[] = '';
+								$dataConcerto[] = '';
+								$oraConcerto[] = '';
+								$compensoPattuitoConcerto[] = '';
+								$compensoEffettivoConcerto[] = '';
+								for($i = 0;$row = mysqli_fetch_assoc($result); $i++)
+								{
+									$nomeLocale[$i] = $row["Nome"];
+									$cittaLocale[$i] = $row["Citta"];
+									$dataConcerto[$i] = $row["Data"];
+									$oraConcerto[$i] = $row["Ora"];
+									$compensoPattuitoConcerto[$i] = $row["CompensoPattuito"];
+									$compensoEffettivoConcerto[$i] = $row["CompensoEffettivo"];
+								}
+							}
+							else
+							{
+							echo "0 results";
+							}
+
+							echo"Eventi in ordine di guadagno decrescente";
+			    			
+			    			for($i=0;$i<$nEventi;$i++)
+							{
+								echo"	<form action=\"storico.php\" method=\"POST\">
+										<details>
+										<summary>".$cittaLocale[$i]." ".$dataConcerto[$i]."</summary>
+										<div>Ora di inizio: ".$oraConcerto[$i]."</div>
+										<div>Locale: ".$nomeLocale[$i]."</div>
+										<div>Compenso pattuito: ".$compensoPattuitoConcerto[$i]."</div>
+										<div>Compenso effettivo ricevuto: ".$compensoEffettivoConcerto[$i]."</div>
+										</details>
+										";
+							}
+
+
+
+							echo"Ordina per guadagno: ";
+							echo"<select name=\"ordinamento\">
+									  <option value=\"casuale\">-casuale-</option>
+							 		  <option value=\"crescente\">Crescente</option>
+									  <option value=\"decrescente\">Decrescente</option>
+								</select>
+									  <input type=\"submit\" name=\"button\" value=\"ricerca\"></form>";
+			    			}
+
+			    			if($ordinamento=="casuale")
+			    			{
+			    				header("location: storico.php");
+			    			}
+			
+	
+		}
 	}
+	
 }
 else
 {
 	header("location: index.php");
 }
 
-	
-
-
 ?>
 
-<script>
-
-		function ordinamentoEventi()
-		{
-			var scelta=document.getElementById('ordinamento').value;
-
-			if(scelta == 'crescente')
-			{
-				alert("sono nell'if");
-				alert(scelta);
-				<?php $_SESSION['ordine'] = 'crescente';?>
-				window.location.href = 'storico.php';
-
-			}		
-			else
-			{
-				alert("sono nell'else");
-				alert(scelta);
-				<?php 
-				if(!isset($_SESSION['ordine']))
-				{
-					$_SESSION['ordine'] = 'decrescente'; 
-				}				
-				?>
-				window.location.href = 'storico.php';
-			}
-
-
-		}
-
-</script>
